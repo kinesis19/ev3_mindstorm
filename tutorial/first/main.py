23490b2f39508c8ab6f5f7aa7e5146040e1254f8
Kinesis <lordkinesis@naver.com>
2022-03-15 22:09:26 +0900

    ev3

---
+++ tutorial/first/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
+Motor(Port.A).run(500) #프로그램은 순차적 구조인데, ev3 모터 2개를 동시에 움직이게 하려면 어떻게 해야 할까?
+Motor(Port.C).run(500)
+wait(1000)
